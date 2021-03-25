6a43d6925ff7afa02ebb3b4488c5a2af7e5621b6
rpache01 <49562889+rpache01@users.noreply.github.com>
2021-03-25 01:11:26 -0500

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Actions, Router, Scene} from "react-native-router-flux"; //install package
+import Glossary from './Screens/Glossary';
+import Handbook from './Screens/Handbook';
 import Home from './Screens/Home';
+import Leadership from './Screens/Leadership';
 import MenuScreen from './Screens/MenuScreen';
 
 export default function App() {
@@ -12,8 +15,10 @@ export default function App() {
        <Scene key="app">
           <Scene key="scene1" component={Home} hideNavBar />
           <Scene key="scene2" component={MenuScreen} hideNavBar/>
+          <Scene key="scene3" component={Handbook} hideNavBar/>
+          <Scene key="scene4" component={Glossary} hideNavBar/>
+          <Scene key="scene5" component={Leadership} hideNavBar/>
       </Scene>
-      
     </Router>
   );
 }
