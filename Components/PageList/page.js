293a17a83ca85a582eee4b3d9cc2024a5e3ebc86
rpache01 <49562889+rpache01@users.noreply.github.com>
2021-03-24 23:04:57 -0500

    Create page.js

---
+++ Components/PageList/page.js
@@ -0,0 +1,11 @@
+export default [{
+   // name: "Texas Military Department",
+    tagline: "Swipe up for tutorial",
+    image: require('../../assets/TMDwallpaper2.jpg'),
+    
+    },
+    {
+    name: "Tutorial",
+    //tagline: "Duty.Honor.Texas",
+    image: require('../../assets/TMDwallpaper4.jpg'),
+        }]
