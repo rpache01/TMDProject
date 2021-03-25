c47fa1f5881aba43c590948594feae4b541e6156
rpache01 <49562889+rpache01@users.noreply.github.com>
2021-03-24 22:46:44 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,28 @@
+//Team 28
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Actions, Router, Scene} from "react-native-router-flux"; //install package
+import Home from './Screens/Home';
+import MenuScreen from './Screens/MenuScreen';
+
+export default function App() {
+  return (
+    <Router>
+       <Scene key="app">
+          <Scene key="scene1" component={Home} hideNavBar />
+          <Scene key="scene2" component={MenuScreen} hideNavBar/>
+      </Scene>
+      
+    </Router>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
