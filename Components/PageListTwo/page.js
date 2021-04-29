08892a86f9501010f281daed5b35860205214583
rpache01 <49562889+rpache01@users.noreply.github.com>
2021-04-29 16:07:02 -0500

    Create page.js

---
+++ Components/PageListTwo/page.js
@@ -0,0 +1,17 @@
+export default [{
+    name: "Table of Contents",
+     //tagline: "Swipe up for tutorial",
+     image: require('../../assets/TMDtoc1.jpg'),
+     
+     },
+     {
+    // name: "Tutorial",
+     //tagline: "",
+     image: require('../../assets/TMDtoc2.jpg'),
+    
+         },
+         {
+            //name: "Tutorial",
+            //tagline: "",
+            image: require('../../assets/TMDtoc3.jpg'),
+                }]
