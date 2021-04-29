47697463d839308e94dbeb7933a585184cc36baa
rpache01 <49562889+rpache01@users.noreply.github.com>
2021-04-29 16:06:34 -0500

    Create styles.js

---
+++ Components/CarItemT/styles.js
@@ -0,0 +1,58 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const styles = StyleSheet.create ({
+    Natcontainer: {
+        width: "100%",
+        height: Dimensions.get('window').height,
+      },
+      titles: { //location of the titles
+        marginTop: "10%",
+        width: "100%",
+        alignItems: 'center',
+      },
+      title: { //how the Texas national guard looks
+        fontSize: 25, //size of the title
+        color: 'black',
+        fontWeight: 'bold',
+      },
+      subtitle: {
+        fontSize: 16,
+        color: 'white',
+        fontWeight: 'bold',
+      },
+      subtitleT: { //tutoral line font, color, and size change
+        fontSize: 16,
+        color: 'white',
+        fontWeight: 'bold',
+        padding: 10,
+      },
+    
+      image: {
+        width: "100%",
+        height: "100%",
+        resizeMode:"cover",
+        position: "absolute",
+      },
+
+      buttonsContainer: {
+        position: "absolute",
+        bottom: 50,
+        width: "100%",
+        alignItems: 'center',
+      },
+      buttonsContainerT: { //Tutorial location
+        position: "absolute",
+        marginVertical: 330,
+        width: "100%",
+        alignItems: 'center',
+      }
+
+
+});
+
+export default styles;
+
+
+
+
+//Roberto Pacheco
