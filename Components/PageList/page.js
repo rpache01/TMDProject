a7841160f112ab609bc123e399d6f92dc09df61f
rpache01 <49562889+rpache01@users.noreply.github.com>
2021-04-29 16:06:36 -0500

    Update page.js

--- Components/PageList/page.js
+++ Components/PageList/page.js
@@ -6,6 +6,10 @@ export default [{
     },
     {
     name: "Tutorial",
-    //tagline: "Duty.Honor.Texas",
-    image: require('../../assets/TMDwallpaper4.jpg'),
+    //tagline: "",
+    image: require('../../assets/TMDwallpaper8.jpg'),
+    linea: "First Tutorial Line",
+    lineb: "Second Tutorial Line",
+    linec: "Third Tutorial Line",
+    linef: "Fourth Tutorial Line",
         }]
