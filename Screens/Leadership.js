e4b31430f84d7d41d4d09b8cf80933f38190715e
rpache01 <49562889+rpache01@users.noreply.github.com>
2021-04-29 16:07:29 -0500

    Update Leadership.js

--- Screens/Leadership.js
+++ Screens/Leadership.js
@@ -8,7 +8,8 @@ import {
     Image,
     ImageBackground,
     ViewBase,
-    Alert
+    Alert,
+    Button
 } from "react-native";
 import {Actions, Router, Scene} from "react-native-router-flux";
 import CarItem from "../Components/CarItem";
@@ -38,18 +39,60 @@ export default class Leadership extends Component {
                     <View style={styles.Natcontainer}>
                     
                     <ImageBackground 
-                    source={require("../assets/TMDwallpaper7.jpg")}
+                    source={require("../assets/TMDwallpaper9.jpg")}
                     style={styles.image}
                     />
 
                     <View style={styles.titles}> 
-                        <Text style={styles.title}>{"Leadership"}</Text>
-                        <Text style={styles.subtitle}>{"Duty.Honor.Texas"}</Text>
-                    </View> 
+                        <Text style={styles.title}>{"Core Aspects of Leadership"}</Text>
 
+                        <Text style={styles.subtitle}>{"Duty.Honor.Texas"}</Text>
+
+                        
+                        
+                    
+                    </View> 
+                    <View style={styles.buttonsContainerLE}>
+                            <StyledButton //first button
+                            type="secondary" //what color is the button 
+                            content={"Soldier Lethality"}  //title of button
+                            onPress={() => {  //what it does on press
+                            Alert.alert("Soldier Lethality");
+                            //Actions.scene3("Handbook")
+                            }}/>
+                            <StyledButton //first button
+                            type="secondary" //what color is the button 
+                            content={"Company Admin"}  //title of button
+                            onPress={() => {  //what it does on press
+                            Alert.alert("Company Admin was pressed");
+                            //Actions.scene3("Handbook")
+                            }}/>
+                            <StyledButton //first button
+                            type="secondary" //what color is the button 
+                            content={"Training Management"}  //title of button
+                            onPress={() => {  //what it does on press
+                            Alert.alert("Handbook was pressed");
+                            //Actions.scene3("Handbook")
+                            }}/>
+                            <StyledButton //first button
+                            type="secondary" //what color is the button 
+                            content={"Talent Management"}  //title of button
+                            onPress={() => {  //what it does on press
+                            Alert.alert("Handbook was pressed");
+                            //Actions.scene3("Handbook")
+                            }}/>
+                            <StyledButton //first button
+                            type="secondary" //what color is the button 
+                            content={"Logistics"}  //title of button
+                            onPress={() => {  //what it does on press
+                            Alert.alert("Handbook was pressed");
+                            //Actions.scene3("Handbook")
+                            }}/>
+                            
+                            
 
                     
-
+                        </View>
 
                     </View>
                 </View>
