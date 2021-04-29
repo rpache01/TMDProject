6c2af169286a38bbb7048fcce827c2b360c4e4e1
rpache01 <49562889+rpache01@users.noreply.github.com>
2021-04-29 16:06:31 -0500

    Create index.js

---
+++ Components/CarItemT/index.js
@@ -0,0 +1,57 @@
+import { createFactory } from "react";
+import {StatusBar} from 'expo-status-bar';
+import React from 'react';
+import {View, Text, ImageBackground} from "react-native"; //importing view and text from react
+import StyledButton from "../StyledButton";
+import styles from './styles';
+
+const CarItemT = (props) => { //arrow function
+
+    const { name, tagline, image, linea, lineb, linec, linef} = props.pages; //these are the different properties for each page made
+                                            // These prop go in text instead of hard coding below
+    return (  //returning a view with just not a component 
+        <View style={styles.Natcontainer}>
+   
+        <ImageBackground 
+        source={image}
+        style={styles.image}
+        />
+     
+       <View style={styles.titles}> 
+         <Text style={styles.title}>{name}</Text>
+         
+       </View> 
+
+
+        <View style={styles.buttonsContainer}>
+          <Text style={styles.subtitle}>{tagline}</Text>
+          
+        </View>
+        <View style={styles.buttonsContainerT}>
+          <Text style={styles.subtitleT}>{linea}</Text>
+          <Text style={styles.subtitleT}>{lineb}</Text>
+          <Text style={styles.subtitleT}>{linec}</Text>
+          <Text style={styles.subtitleT}>{linef}</Text>
+          
+        </View>
+     </View> //type = primary is sending information/properties from parent to child component
+     
+    );
+
+}; 
+
+export default CarItemT;
+          /* <StyledButton //first button
+            type="primary" //what color is the button 
+            content={"Handbook"}  //title of button
+            onPress={() => {  //what it does on press
+              console.warn("Handbook was pressed");
+            }}
+            />   
+
+            <StyledButton // second button
+            type="secondary" 
+            content={"Glossary"} 
+            onPress={() => {
+              console.warn("Glossary was pressed") 
+            }} */
